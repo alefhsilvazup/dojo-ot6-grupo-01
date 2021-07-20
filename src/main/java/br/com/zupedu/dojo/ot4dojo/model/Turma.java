1e3d6051e52c0a1dfba8e896acfcc8f977a8c000
achiley.pinto <achileyd@gmail.com>
2021-07-20 11:38:51 -0300

    Piloto: Achiley Copiloto: Ana

---
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/model/Turma.java
@@ -0,0 +1,38 @@
+package br.com.zupedu.dojo.ot4dojo.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="turmas")
+public class Turma {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Size(max=50)
+	@Column(unique = true)
+	private String nome;
+	@Column(unique = true)
+	private LocalDate iniciaEm;
+	@Column(unique = true)
+	private LocalDate terminaEm;
+	
+	@Deprecated
+	public Turma() {
+		
+	}
+	public Turma(@Size(max = 50) String nome, LocalDate iniciaEm, LocalDate terminaEm) {
+		this.nome = nome;
+		this.iniciaEm = iniciaEm;
+		this.terminaEm = terminaEm;
+	}
+	
+	
+}
